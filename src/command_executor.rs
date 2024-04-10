6358c02dfba5620deb7b2d05334f74da37a287ca
AlphaRora <adoba@alpharora.com>
2024-04-10 01:22:00 -0400

    nice

--- src/command_executor.rs
+++ src/command_executor.rs
@@ -9,8 +9,22 @@ pub fn execute_bash_command(request_successful: bool) -> Result<String, std::io:
         println!("Request to Cloudflare Worker was successful. Printing something else.");
     }
 
-    let command = r#"interval=5;process_name="tritonserver --model-repository=/mnt/models";pid=$(pgrep -f "$process_name");if [[ -z "$pid" ]]; thenecho "Error: Inference process not found. Please provide the correct process name.";exit 1;fi;echo "Monitoring wait time for processes targets: $process_name (PID: $pid)";echo "---------------------------------------------------------";while true; doiostat -d -x 1 $interval | tail -n +3;pidstat -d -p $pid $interval | tail -n +4 | awk '{print "I/O Wait (%): " $11}';echo "---------------------------------------------------------";done"#;
-
+    let command = r#"
+        interval=5;
+        process_name="tritonserver --model-repository=/mnt/models";
+        pid=$(pgrep -f "$process_name");
+        if [[ -z "$pid" ]]; then
+            echo "Error: Inference process not found. Please provide the correct process name.";
+            exit 1;
+        fi;
+        echo "Monitoring wait time for processes targets: $process_name (PID: $pid)";
+        echo "---------------------------------------------------------";
+        while true; do
+            iostat -d -x 1 $interval | tail -n +3;
+            pidstat -d -p $pid $interval | tail -n +4 | awk '{print "I/O Wait (%): " $11}';
+            echo "---------------------------------------------------------";
+        done
+    "#;
     let output = Command::new("bash")
         .arg("-c")
         .arg(command)
