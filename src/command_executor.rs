cd906375c7a9e47448c30be2d36e626ade21b390
AlphaRora <adoba@alpharora.com>
2024-04-23 16:46:19 -0400

    nice

--- src/command_executor.rs
+++ src/command_executor.rs
@@ -4,16 +4,22 @@ use std::sync::mpsc::Sender;
 use std::io::{BufRead, BufReader};
 
 pub fn execute_bash_command(tx: Sender<String>, lscpu_tx: Sender<String>) {  
-    // New lscpu command execution  
-    let output = Command::new("lscpu")  
-        .output()  
-        .expect("Failed to execute lscpu command");  
+    let mut lscpu_child = Command::new("lscpu")  
+        .stdout(Stdio::piped())  
+        .spawn()  
+        .expect("Failed to spawn lscpu child process");  
   
-    // Check for errors from the lscpu command  
-    if output.status.success() {  
-        let lscpu_output_str = String::from_utf8_lossy(&output.stdout);  
-        lscpu_tx.send(lscpu_output_str.to_string()).expect("Failed to send lscpu command output");  
-    } else {  
-        eprintln!("lscpu command failed: {}", String::from_utf8_lossy(&output.stderr));  
+    let lscpu_stdout = lscpu_child.stdout.take().expect("Failed to get lscpu child stdout");  
+    let lscpu_stdout_reader = BufReader::new(lscpu_stdout);  
+  
+    for line in lscpu_stdout_reader.lines() {  
+        match line {  
+            Ok(lscpu_output) => {  
+                if let Err(e) = lscpu_tx.send(lscpu_output) {  
+                    eprintln!("Failed to send lscpu command output: {}", e);  
+                }  
+            }  
+            Err(e) => eprintln!("Failed to read line from lscpu child stdout: {}", e),  
+        }  
     }  
 }  
