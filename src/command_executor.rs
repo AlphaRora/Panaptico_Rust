70cf6995a4b3adbb11d093c08f0c32205798e1ad
AlphaRora <adoba@alpharora.com>
2024-04-23 16:34:17 -0400

    nice

--- src/command_executor.rs
+++ src/command_executor.rs
@@ -5,18 +5,15 @@ use std::io::{BufRead, BufReader};
 
 pub fn execute_bash_command(tx: Sender<String>, lscpu_tx: Sender<String>) {  
     // New lscpu command execution  
-    let lscpu_output = Command::new("lscpu")  
+    let output = Command::new("lscpu")  
         .output()  
         .expect("Failed to execute lscpu command");  
   
-    // The output is a Result type, we need to handle it  
-    match lscpu_output {  
-        Ok(output) => {  
-            let lscpu_output_str = String::from_utf8_lossy(&output.stdout);  
-            lscpu_tx.send(lscpu_output_str.to_string()).expect("Failed to send lscpu command output");  
-        },  
-        Err(e) => {  
-            eprintln!("lscpu command failed: {}", e);  
-        }  
+    // Check for errors from the lscpu command  
+    if output.status.success() {  
+        let lscpu_output_str = String::from_utf8_lossy(&output.stdout);  
+        lscpu_tx.send(lscpu_output_str.to_string()).expect("Failed to send lscpu command output");  
+    } else {  
+        eprintln!("lscpu command failed: {}", String::from_utf8_lossy(&output.stderr));  
     }  
 }  
