1488592e0d7072c6443972ba4a464e178acd2f76
AlphaRora <adoba@alpharora.com>
2024-04-23 21:35:38 -0400

    ha

--- src/worker_communication.rs
+++ src/worker_communication.rs
@@ -1,15 +1,16 @@
 use reqwest::Client;  
   
-pub async fn send_data_request(worker_url: &str, data: &str) -> Result<String, reqwest::Error> {  
-    let client = Client::new();  
-    let response = client  
-        .post(worker_url)  
-        .header("Content-Type", "text/plain")  
-        .body(data.to_owned())  
-        .send()  
-        .await?  
-        .text()  
-        .await?;  
-  
-    Ok(response)  
-}  
+pub async fn send_data_request(worker_url: &str, data: &str) -> Result<String, reqwest::Error> {
+    let client = Client::new();
+    let response = client
+        .post(worker_url)
+        .header("Content-Type", "text/plain")
+        .body(data.to_owned())
+        .send()
+        .await?
+        .text()
+        .await?;
+
+    println!("Worker response: {}", response);
+    Ok(response)
+}
