666f4945e44facbf7a27c4b836b0c68d6ca135bf
AlphaRora <adoba@alpharora.com>
2024-04-24 17:11:45 -0400

    sign

--- src/worker_communication.rs
+++ src/worker_communication.rs
@@ -14,3 +14,18 @@ pub async fn send_data_request(worker_url: &str, data: &str) -> Result<String, r
     println!("Worker response: {}", response);
     Ok(response)
 }
+
+pub async fn send_glances_data_request(glances_url: &str, data: &str) -> Result<String, reqwest::Error> {
+    let client = Client::new();
+    let response = client
+        .post(glances_url)
+        .header("Content-Type", "text/plain")
+        .body(data.to_owned())
+        .send()
+        .await?
+        .text()
+        .await?;
+
+    println!("Glances Worker response: {}", response);
+    Ok(response)
+}
