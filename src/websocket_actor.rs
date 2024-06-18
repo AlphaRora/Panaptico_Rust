06827e89c0677045026e94222333c9bcab9e02ef
AlphaRora <adoba@alpharora.com>
2024-06-18 14:34:57 -0400

    tip

--- src/websocket_actor.rs
+++ src/websocket_actor.rs
@@ -1,33 +1,4 @@
-use actix::*;
-use actix_web_actors::ws;
-use tokio::net::TcpStream;
-use tokio_tungstenite::WebSocketStream;
 
-pub struct WebSocketActor {
-    ws_stream: WebSocketStream<TcpStream>,
-}
-
-impl Actor for WebSocketActor {
-    type Context = ws::WebsocketContext<Self>;
-
-    fn started(&mut self, ctx: &mut Self::Context) {
-        println!("WebSocket actor started");
-    }
-}
-
-impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for WebSocketActor {
-    fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
-        match msg {
-            Ok(ws::Message::Text(text)) => {
-                ctx.text(text);
-            }
-            Ok(ws::Message::Binary(bin)) => {
-                ctx.binary(bin);
-            }
-            _ => (),
-        }
-    }
-}
 use actix::*;
 use actix_web_actors::ws;
 use tokio::net::TcpStream;
