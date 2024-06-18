fa995fb0f20cdae919e514b1dd83aa71dae8a719
AlphaRora <adoba@alpharora.com>
2024-06-18 15:36:28 -0400

    tip

--- src/websocket_actor.rs
+++ src/websocket_actor.rs
@@ -1,36 +1,32 @@
-use actix::*;  
-use actix_web_actors::ws;  
-use tokio::net::TcpStream;  
-use tokio_tungstenite::WebSocketStream;  
-  
-pub struct WebSocketActor {  
-    ws_stream: WebSocketStream<TcpStream>,  
-}  
-  
-impl WebSocketActor {  
-    pub fn new(ws_stream: WebSocketStream<TcpStream>) -> Self {  
-        WebSocketActor { ws_stream }  
-    }  
-}  
-  
-impl Actor for WebSocketActor {  
-    type Context = ws::WebsocketContext<Self>;  
-  
-    fn started(&mut self, ctx: &mut Self::Context) {  
-        println!("WebSocket actor started");  
-    }  
-}  
-  
-impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for WebSocketActor {  
-    fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {  
-        match msg {  
-            Ok(ws::Message::Text(text)) => {  
-                ctx.text(text);  
-            }  
-            Ok(ws::Message::Binary(bin)) => {  
-                ctx.binary(bin);  
-            }  
-            _ => (),  
-        }  
-    }  
-}  
+use actix::*;
+use actix_web_actors::ws;
+
+pub struct WebSocketActor;
+
+impl WebSocketActor {
+    pub fn new() -> Self {
+        WebSocketActor
+    }
+}
+
+impl Actor for WebSocketActor {
+    type Context = ws::WebsocketContext<Self>;
+
+    fn started(&mut self, _ctx: &mut Self::Context) {
+        println!("WebSocket actor started");
+    }
+}
+
+impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for WebSocketActor {
+    fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
+        match msg {
+            Ok(ws::Message::Text(text)) => {
+                ctx.text(text);
+            }
+            Ok(ws::Message::Binary(bin)) => {
+                ctx.binary(bin);
+            }
+            _ => (),
+        }
+    }
+}
