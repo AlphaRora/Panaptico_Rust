78f9ee95da2c6b79144ebc5d7374cb9093561fb1
AlphaRora <adoba@alpharora.com>
2024-06-18 14:44:06 -0400

    tip

--- src/supervisor.rs
+++ src/supervisor.rs
@@ -1,22 +1,21 @@
-use actix::prelude::*;
-use actix::{Actor, Context, Supervised, Supervisor, System};
-
-pub struct SupervisorActor;
-
-impl Actor for SupervisorActor {
-    type Context = Context<Self>;
-}
-
-impl Supervised for SupervisorActor {
-    fn restarting(&mut self, _: &mut Self::Context) {
-        println!("Supervisor is restarting");
-    }
-}
-
-impl Supervisor for SupervisorActor {}
-
-impl SupervisorActor {
-    pub fn start_supervisor() {
-        let _ = Supervisor::start(move |_| SupervisorActor);
-    }
-}
+use actix::prelude::*;  
+  
+pub struct SupervisorActor;  
+  
+impl Actor for SupervisorActor {  
+    type Context = Context<Self>;  
+}  
+  
+impl Supervised for SupervisorActor {  
+    fn restarting(&mut self, _: &mut Self::Context) {  
+        println!("Supervisor is restarting");  
+    }  
+}  
+  
+impl Supervisor for SupervisorActor {}  
+  
+impl SupervisorActor {  
+    pub fn start_supervisor() {  
+        let _ = SupervisorActor.start();  
+    }  
+}  
