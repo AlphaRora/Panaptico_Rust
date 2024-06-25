20d1d90151d55f959eec9284e5e3acdde1e74d66
AlphaRora <adoba@alpharora.com>
2024-06-25 09:51:17 -0400

    snag

--- src/main.rs
+++ src/main.rs
@@ -31,29 +31,29 @@ async fn main() -> std::io::Result<()> {
   
     // Command actors initialization  
     let (bash_tx, bash_rx) = std::sync::mpsc::channel();  
-    let (glances_tx, glances_rx) = std::sync::mpsc::channel();  
+    // let (glances_tx, glances_rx) = std::sync::mpsc::channel();  
     let (num_procs_tx, num_procs_rx) = std::sync::mpsc::channel();  
     let (top_proc_tx, top_proc_rx) = std::sync::mpsc::channel();  
     let (proc_list_tx, proc_list_rx) = std::sync::mpsc::channel();  
     let (load_list_tx, load_list_rx) = std::sync::mpsc::channel();  
-    let (speed_list_tx, speed_list_rx) = std::sync::mpsc::channel();  
+    // let (speed_list_tx, speed_list_rx) = std::sync::mpsc::channel();  
   
     BashCommandActor::new(bash_tx, Arc::clone(&azure_client)).start();  
-    GlancesCommandActor::new(glances_tx, Arc::clone(&azure_client)).start();  
+    // GlancesCommandActor::new(glances_tx, Arc::clone(&azure_client)).start();  
     NumberOfProcessesCommandActor::new(num_procs_tx, Arc::clone(&azure_client)).start();  
     TopProcessCommandActor::new(top_proc_tx, Arc::clone(&azure_client)).start();  
     ProcessListCommandActor::new(proc_list_tx, Arc::clone(&azure_client)).start();  
     NetworkLoadCommandActor::new(load_list_tx, Arc::clone(&azure_client)).start();  
-    NetworkSpeedCommandActor::new(speed_list_tx, Arc::clone(&azure_client)).start();  
+    // NetworkSpeedCommandActor::new(speed_list_tx, Arc::clone(&azure_client)).start();  
   
     // Spawn tasks to handle the outputs from each command actor  
     actix_rt::spawn(handle_bash_output(bash_rx, Arc::clone(&azure_client)));  
-    actix_rt::spawn(handle_glances_output(glances_rx, Arc::clone(&azure_client)));  
+    // actix_rt::spawn(handle_glances_output(glances_rx, Arc::clone(&azure_client)));  
     actix_rt::spawn(handle_num_procs_output(num_procs_rx, Arc::clone(&azure_client)));  
     actix_rt::spawn(handle_top_proc_output(top_proc_rx, Arc::clone(&azure_client)));  
     actix_rt::spawn(handle_proc_list_output(proc_list_rx, Arc::clone(&azure_client)));  
     actix_rt::spawn(handle_load_output(load_list_rx, Arc::clone(&azure_client)));  
-    actix_rt::spawn(handle_speed_output(speed_list_rx, Arc::clone(&azure_client)));  
+    // actix_rt::spawn(handle_speed_output(speed_list_rx, Arc::clone(&azure_client)));  
   
     // Start HTTP server for WebSocket connections  
     HttpServer::new(move || {  
@@ -74,12 +74,12 @@ async fn handle_bash_output(bash_rx: std::sync::mpsc::Receiver<String>, azure_cl
   
 // ... (the rest of the functions remain the same)  
   
-async fn handle_glances_output(glances_rx: std::sync::mpsc::Receiver<String>, azure_client: Arc<AzureDataLakeClient>) {  
-    for command_output in glances_rx {  
-        println!("Output from glances command: {}", command_output);  
-        azure_client.upload("glances_output.txt", &command_output).await.unwrap();  
-    }  
-}  
+// async fn handle_glances_output(glances_rx: std::sync::mpsc::Receiver<String>, azure_client: Arc<AzureDataLakeClient>) {  
+//     for command_output in glances_rx {  
+//         println!("Output from glances command: {}", command_output);  
+//         azure_client.upload("glances_output.txt", &command_output).await.unwrap();  
+//     }  
+// }  
   
 async fn handle_num_procs_output(num_procs_rx: std::sync::mpsc::Receiver<String>, azure_client: Arc<AzureDataLakeClient>) {  
     for command_output in num_procs_rx {  
@@ -109,9 +109,9 @@ async fn handle_load_output(load_rx: std::sync::mpsc::Receiver<String>, azure_cl
     }  
 }  
   
-async fn handle_speed_output(speed_rx: std::sync::mpsc::Receiver<String>, azure_client: Arc<AzureDataLakeClient>) {  
-    for command_output in speed_rx {  
-        println!("Network speed: {}", command_output);  
-        azure_client.upload("speed_output.txt", &command_output).await.unwrap();  
-    }  
-}  
+// async fn handle_speed_output(speed_rx: std::sync::mpsc::Receiver<String>, azure_client: Arc<AzureDataLakeClient>) {  
+//     for command_output in speed_rx {  
+//         println!("Network speed: {}", command_output);  
+//         azure_client.upload("speed_output.txt", &command_output).await.unwrap();  
+//     }  
+// }  
